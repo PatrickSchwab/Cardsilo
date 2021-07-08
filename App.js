a04f0220fabb1764df595886723c1604004804ef
Patrick Schwab <57998866+PatrickSchwab@users.noreply.github.com>
2021-07-08 11:21:56 +0200

    Finished Home Component with list and logo alternation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { StyleSheet, Text, View } from "react-native";
 import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -12,53 +13,84 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
 
-    const [cardList, setCardList] = useState([{
-        companyName: "Tesla",
-        notes: "blablabla",
-    }])
+    const [cardList, setCardList] = useState([
+        {
+            id: 0,
+            companyName: "Tesla",
+            notes: "blablabla",
+        },
+        {
+            id: 1,
+            companyName: "Google",
+            notes: "blablabla",
+        },
+        {
+            id: 2,
+            companyName: "Pepsi",
+            notes: "blablabla",
+        },
+        {
+            id: 3,
+            companyName: "Migros",
+            notes: "blablabla",
+        },
+        {
+            id: 4,
+            companyName: "Coop",
+            notes: "blablabla",
+        }
+        ,
+        {
+            id: 5,
+            companyName: "brack",
+            notes: "blablabla",
+        }
+    ])
 
     return (
       <>
-        <StatusBar/>
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+          <NativeBaseProvider>
+            <StatusBar/>
+            <NavigationContainer>
+                <Tab.Navigator
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
 
-                        if (route.name === 'Home') {
-                            iconName = focused
-                                ? 'ios-information-circle'
-                                : 'ios-information-circle-outline';
-                            return <MaterialCommunityIcons name="home" size={24} color={color}/>;
-                        } else if (route.name === 'Settings') {
-                            iconName = focused ? 'ios-list-box' : 'ios-list';
-                            return <MaterialCommunityIcons name="account-settings" size={24} color={color} />;
-                        } else if (route.name === 'Create') {
-                            iconName = focused ? 'ios-list-box' : 'ios-list';
-                            return <MaterialCommunityIcons name="camera-plus-outline" size={24} color={color} />;
-                        }
-                    },
-                })}
-                tabBarOptions={{
-                    activeTintColor: 'tomato',
-                    inactiveTintColor: 'gray',
-                }}
-            >
-            <Tab.Screen
-                name="Home"
-                children={()=><Home cardList={cardList}/>}
-            />
-            <Tab.Screen
-                name="Create"
-                children={()=><Create cardList={cardList}/>}
-            />
-            <Tab.Screen
-                name="Settings"
-                children={()=><Settings cardList={cardList}/>}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
+                            if (route.name === 'Home') {
+                                iconName = focused
+                                    ? 'ios-information-circle'
+                                    : 'ios-information-circle-outline';
+                                return <MaterialCommunityIcons name="home" size={24} color={color}/>;
+                            } else if (route.name === 'Settings') {
+                                iconName = focused ? 'ios-list-box' : 'ios-list';
+                                return <MaterialCommunityIcons name="account-settings" size={24} color={color} />;
+                            } else if (route.name === 'Create') {
+                                iconName = focused ? 'ios-list-box' : 'ios-list';
+                                return <MaterialCommunityIcons name="camera-plus-outline" size={24} color={color} />;
+                            }
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: 'tomato',
+                        inactiveTintColor: 'gray',
+                    }}
+                >
+                <Tab.Screen
+                    name="Home"
+                    children={()=><Home cardList={cardList}/>}
+                />
+                <Tab.Screen
+                    name="Create"
+                    children={()=><Create cardList={cardList}/>}
+                />
+                <Tab.Screen
+                    name="Settings"
+                    children={()=><Settings cardList={cardList}/>}
+                />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </NativeBaseProvider>
       </>
     );
 }
--- Components/Home.js
+++ Components/Home.js
@@ -1,28 +1,75 @@
-import React from 'react';
-import {StyleSheet, Text, View} from "react-native";
-import {Image, Box, Center, HStack, Stack, NativeBaseProvider, FlatList} from "native-base";
+import React, {useState} from 'react';
+import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Image, Divider, Box, Center, HStack, Stack, NativeBaseProvider, FlatList} from "native-base";
 
-const Card = () => {
+const Card = (props) => {
+
+    const [imageLoading, setImageLoading] = useState(true);
+
     return(
-        <View>
+        <TouchableOpacity
+            style={styles.cardComp}
+        >
             <Image
-                source={{
-                    uri: 'https://logo.clearbit.com/tesla.com',
-                }}
-                alt="Alternate Text"
-                size={"md"}
+                source = { imageLoading
+                    ?
+                    { uri: 'https://logo.clearbit.com/'+props.cardList[props.item.id].companyName.toLowerCase()+'.ch?size=500' }
+                    :
+                    { uri: 'https://logo.clearbit.com/'+props.cardList[props.item.id].companyName.toLowerCase()+'.com?size=500' }
+                }
+                onError={()=>setImageLoading(false)}
+                alt={props.cardList[props.item.id].companyName}
+                height={118}
+                resizeMode="cover"
+                borderRadius={9}
             />
-        </View>
+        </TouchableOpacity>
     );
 }
 
 export const Home = (props) => {
+
+    const cardList = props.cardList;
+
     return (
         <>
-
+            <View style={styles.titleCardContainer}>
+                <Text style={styles.titleCardText}>Cards</Text>
+                <Divider/>
+            </View>
+            <View style={styles.flatListContainer}>
+                <FlatList
+                    data={cardList}
+                    numColumns={2}
+                    renderItem={({item}) => (<Card item={item} cardList={cardList}/>)}
+                    keyExtractor={item => item.id}
+                />
+            </View>
         </>
     );
 }
 
 const styles = StyleSheet.create({
+    cardComp : {
+        width : 150,
+        height : 120,
+        marginLeft : 15,
+        marginTop: 30,
+        borderColor : '#707070',
+        borderWidth : 1,
+        borderRadius : 10,
+    },
+    flatListContainer : {
+        marginLeft: 15,
+        height : 600,
+    },
+    titleCardContainer : {
+        marginTop: 100,
+        marginLeft: 30,
+        marginRight: 30,
+    },
+    titleCardText : {
+        fontSize : 32,
+        fontWeight: "bold",
+    }
 });
